01e9c7b747100c191a1598c6b899849d87a65e6e
Mia <mia@navigator.local>
2025-12-26 13:46:36 +0000

    chore: update base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
  
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/polycule-graph/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
